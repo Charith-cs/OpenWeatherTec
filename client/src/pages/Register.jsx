141911c74375e02e6b1884e51ee3f74fbee57036
Charith Sankalpa <84385440+Charith-cs@users.noreply.github.com>
2024-09-24 01:57:49 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -115,11 +115,11 @@ export default function Register() {
                             </div>
                             <div className="mt-2">
                                 <input
-                                    id="confirmPasword"
-                                    name="confirmPasword"
+                                    id="confirmPassword"
+                                    name="confirmPassword"
                                     type="password"
                                     required
-                                    ref={confirmPasword}
+                                    ref={confirmPassword}
                                     className="block p-5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
                                 />
                             </div>
