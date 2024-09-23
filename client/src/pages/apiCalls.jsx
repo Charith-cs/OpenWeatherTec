78bfddfd2d33d2c9d12e37cf477315fa1b541501
Charith Sankalpa <84385440+Charith-cs@users.noreply.github.com>
2024-09-24 01:21:00 +0530

    Update apiCalls.jsx

--- client/src/pages/apiCalls.jsx
+++ client/src/pages/apiCalls.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginCall = async (userCredentials , dispatch) => {
     dispatch({type:'LOGIN_START'});
     try{
-        const res = await axios.post("/api/user/login" , userCredentials);
+        const res = await axios.post("https://open-weather-tec.vercel.app/api/user/login" , userCredentials);
         dispatch({type:"LOGIN_SUCCESS" , payload:res.data});
     }catch(error){
         const errorMessage = error.response ? error.response.data.message : "An error occurred during login";
